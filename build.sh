0aedfd69c9aeb76895f5a71b886bbe9c251d5196
Tiffany Fung <46277006+changanmoon@users.noreply.github.com>
2024-03-19 15:45:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ applyPatches() {
     bash $BL/patch.sh $BL trebledroid
     echo
 
-    echo "--> Applying Poncespersonal patches"
+    echo "--> Applying Ponces's personal patches"
     bash $BL/patch.sh $BL personal
     echo
 
